114a33accbb665583d38a03f71ca82554ea71eae
Max Reinsch <Maxflr33@outlook.com>
2024-05-29 21:06:16 +0100

    V1
    V1

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Notes for Nick
-This is your standard Express server I assume.
-Not meaning to tell you how to suck eggs but: 
-- install npm and express
-- run nodemon
 
-Most pages are obviously placeholders. 
-
-If you could give me some feedback on the folder structure, the index.js Server and anything else you feel is worth pointing out.
-
-Thanks Nick, talk soon. 
--- index.js
+++ index.js
@@ -12,19 +12,6 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-// Render home directory with index.ejs file
-app.get("/about", (req, res) => {
-  res.render("about.ejs");
-});
-
-app.get("/work", (req, res) => {
-  res.render("work.ejs");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
-});
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
